7eb51ce440a2fad6639d8ee2fef67df07dcbbbe7
dannyela <dannyela.denny@tjro.jus.br>
2024-08-14 12:11:31 -0400

    marcação de capítulos de vídeo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import "@vidstack/react/player/styles/default/theme.css";
 import "@vidstack/react/player/styles/default/layouts/video.css";
-import { MediaPlayer, MediaProvider } from "@vidstack/react";
+import { MediaPlayer, MediaProvider, MediaPlayerInstance } from "@vidstack/react";
 import {
   defaultLayoutIcons,
   DefaultVideoLayout,
@@ -15,7 +15,8 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+
 
 interface Video {
   id: number;
@@ -28,7 +29,7 @@ interface Video {
 const videos: Video[] = [
   {
     id: 1,
-    url: "https://minio.tjro.jus.br/dev-cejusc/2024/05/30/Cafe%CC%81%20com%20Inovac%CC%A7a%CC%83o%20-%20Melhore%20sua%20vida%20com%20criatividade%20e%20inovac%CC%A7a%CC%83o.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=LUTEVNN0EWDNRQB4A3TU%2F20240812%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240812T115846Z&X-Amz-Expires=43198&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiJMVVRFVk5OMEVXRE5SUUI0QTNUVSIsImF0X2hhc2giOiJNV1ZNcHl4UTE4RmNtdlRpZ2hObVBBIiwiYXVkIjoibWluaW8iLCJhdXRoX3RpbWUiOjE3MjI2MTQ1NTksImF6cCI6Im1pbmlvIiwiZW1haWwiOiJnaW92YW5pZmVybmFuZGVzQHRqcm8uanVzLmJyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTcyMzQ2NzUxNSwiZmFtaWx5X25hbWUiOiJGZXJuYW5kZXMgZG9zIFNhbnRvcyBPbGl2ZWlyYSIsImdpdmVuX25hbWUiOiJHaW92YW5pIEZlcm5hbmRlcyBkb3MgU2FudG9zIE9saXZlaXJhIiwiaWF0IjoxNzIzNDYzOTE2LCJpc3MiOiJodHRwczovL3Nzby50anJvLmp1cy5ici9hdXRoL3JlYWxtcy9QSlJPIiwianRpIjoiMzhhMzA0ZDQtZjUxMS00NWVkLTljYTEtZDNiM2NmNDc1MzQxIiwibmFtZSI6Ikdpb3ZhbmkgRmVybmFuZGVzIGRvcyBTYW50b3MgT2xpdmVpcmEgRmVybmFuZGVzIGRvcyBTYW50b3MgT2xpdmVpcmEiLCJwb2xpY3kiOiJjb25zb2xlRGV2IiwicHJlZmVycmVkX3VzZXJuYW1lIjoiNTAwMTY0Iiwic2Vzc2lvbl9zdGF0ZSI6IjVlZDk0MjEwLTU4NGQtNDkwMC1hYzZlLThlYjkzZTQ5YjIxYyIsInNpZCI6IjVlZDk0MjEwLTU4NGQtNDkwMC1hYzZlLThlYjkzZTQ5YjIxYyIsInN1YiI6IjhkZDNkMWZlLTI3NDUtNDNjNC1hYjAzLWFkOWI1ZDc2ODMzNyIsInR5cCI6IklEIn0.7YR8aid2_tVPwwxVcp1Djmb6Whyd366SspEGGay8sPu25EbNAXkXuL1yFHFBjFHqOWANGdAAUOJISANmdWUPkQ&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=43c9154faba6d6104f1ad739a6281aea0dc42908cdd353224b6f96e3c61cd920",
+    url: "https://minio.tjro.jus.br/dev-cejusc/2024/05/30/Cafe%CC%81%20com%20Inovac%CC%A7a%CC%83o%20-%20Melhore%20sua%20vida%20com%20criatividade%20e%20inovac%CC%A7a%CC%83o.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=2A0EDP0DJ54UBFPHQF0O%2F20240814%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240814T153428Z&X-Amz-Expires=43200&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiIyQTBFRFAwREo1NFVCRlBIUUYwTyIsImF0X2hhc2giOiJlbTRVaVZJbW5IOXhNbUNlSDlnVWxnIiwiYXVkIjoibWluaW8iLCJhdXRoX3RpbWUiOjE3MjM2NDAyNjAsImF6cCI6Im1pbmlvIiwiZW1haWwiOiJnaW92YW5pZmVybmFuZGVzQHRqcm8uanVzLmJyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTcyMzY1MzI0OCwiZmFtaWx5X25hbWUiOiJGZXJuYW5kZXMgZG9zIFNhbnRvcyBPbGl2ZWlyYSIsImdpdmVuX25hbWUiOiJHaW92YW5pIEZlcm5hbmRlcyBkb3MgU2FudG9zIE9saXZlaXJhIiwiaWF0IjoxNzIzNjQ5NjUwLCJpc3MiOiJodHRwczovL3Nzby50anJvLmp1cy5ici9hdXRoL3JlYWxtcy9QSlJPIiwianRpIjoiNzU5MzEwNzgtMjQyZS00YmI0LWFlYTUtNWFiYzJiMjYwZDkxIiwibmFtZSI6Ikdpb3ZhbmkgRmVybmFuZGVzIGRvcyBTYW50b3MgT2xpdmVpcmEgRmVybmFuZGVzIGRvcyBTYW50b3MgT2xpdmVpcmEiLCJwb2xpY3kiOiJjb25zb2xlRGV2IiwicHJlZmVycmVkX3VzZXJuYW1lIjoiNTAwMTY0Iiwic2Vzc2lvbl9zdGF0ZSI6IjViODIzMTA0LTRmYzItNDJlMy04ODYyLTExMTE3MWZhYWNlMSIsInNpZCI6IjViODIzMTA0LTRmYzItNDJlMy04ODYyLTExMTE3MWZhYWNlMSIsInN1YiI6IjhkZDNkMWZlLTI3NDUtNDNjNC1hYjAzLWFkOWI1ZDc2ODMzNyIsInR5cCI6IklEIn0.xRrimxU06GH8Y9opwACSyb857UfXqRYfTpjxb8k9pEqkyGQ3nSq2wzzeWUGcz3pODocB2DdMg0Q4ADIOU2Ob1g&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=be57589ec40817a983a94e12f6b08077a6420d441f52a0230bbc1b59f06ab7be",
     title: "Café com Inovação",
     cardTitle: "Café com Inovação",
     cardDescription: "Tribunal Justiça do Estado de Rondônia",
@@ -69,6 +70,14 @@ const Home: React.FC<HomeProps> = ({ searchParams }: HomeProps) => {
       setIsValidUrl(false);
     }
   }, [query]);
+  
+  const mediaPlayerRef = useRef<MediaPlayerInstance | null>(null);
+
+  const jumpToTimestamp = (time: number) => {
+    if (mediaPlayerRef.current) {
+      mediaPlayerRef.current.currentTime = time;
+    }
+  };
 
   return (
     <div className="flex flex-1 items-center justify-center">
@@ -87,11 +96,10 @@ const Home: React.FC<HomeProps> = ({ searchParams }: HomeProps) => {
           <p>Resultado não encontrado</p>
         </div>
       ) : 
-
       query !== "" && isValidUrl && (
         <div className="grid lg:grid-cols-2 w-8/12 gap-4 items-stretch">
           <div>
-            <MediaPlayer
+            <MediaPlayer ref = {mediaPlayerRef}
               className="h-full"
               title="Café com Inovação"
               src={query}
@@ -110,11 +118,15 @@ const Home: React.FC<HomeProps> = ({ searchParams }: HomeProps) => {
                 <CardDescription>
                   Tribunal Justiça do Estado de Rondônia
                 </CardDescription>
+                <div className="flex gap-3"> <button className="bg-gray-100 border p-1" onClick={() => jumpToTimestamp(90)}> 1:30</button>
+                  <button className="bg-gray-100 border p-1" onClick={() => jumpToTimestamp(120)}> 2:00</button>
+                  <button className="bg-gray-100 border p-1" onClick={() => jumpToTimestamp(150)}> 2:30</button>
+                  </div>
               </CardHeader>
               <ScrollArea className="h-[250px] text-justify">
                 <CardContent>
                   <p className="font-bold">Transcrição do vídeo</p>
-                   <p>Plantei uma roseira O vento no cume bate A rosa no cume cheira, Quando vem a chuva fina Salpicos no cume caem Formigas no cume entram, Abelhas do cume saem Quando cai a chuva grossa A água do cume desce O barro do cume escorre O mato no cume cresce Então, quando cessa a chuva No cume volta a alegria Pois torna a brilhar de novo O Sol que no cume ardia No alto daquele cume Plantei uma roseira O vento no cume bate A rosa no cume cheira Quando vem a chuva fina Salpicos no cume caem Formigas no cume entram Abelhas do cume saem Quando cai a chuva grossa A água do cume desce O barro do cume escorre O mato no cume cresce Então, quando cessa a chuva No cume volta a alegria Pois torna a brilhar de novo O Sol que no cume ardia Pois torna a brilhar de novo O Sol que no cume ardia Pois torna a brilhar de novo OSol que no cume ardia</p>
+                  <p>Plantei uma roseira O vento no cume bate A rosa no cume cheira, Quando vem a chuva fina Salpicos no cume caem Formigas no cume entram, Abelhas do cume saem Quando cai a chuva grossa A água do cume desce O barro do cume escorre O mato no cume cresce Então, quando cessa a chuva No cume volta a alegria Pois torna a brilhar de novo O Sol que no cume ardia No alto daquele cume Plantei uma roseira O vento no cume bate A rosa no cume cheira Quando vem a chuva fina Salpicos no cume caem Formigas no cume entram Abelhas do cume saem Quando cai a chuva grossa A água do cume desce O barro do cume escorre O mato no cume cresce Então, quando cessa a chuva No cume volta a alegria Pois torna a brilhar de novo O Sol que no cume ardia Pois torna a brilhar de novo O Sol que no cume ardia Pois torna a brilhar de novo OSol que no cume ardia</p><a href="/">00:25</a>
                   <p>{videos.find((video) => video.url === query)?.cardContent}</p>
                 </CardContent>
               </ScrollArea>
